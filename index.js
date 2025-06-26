d70c3260abbba05fd44e68b12be861fd483aec98
boyratata <152520757+boyratata@users.noreply.github.com>
2025-06-26 22:26:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-(function(l,r,a,d,o,_,g,u){"use strict";const n=[],c=r.findByProps("_channelMessages"),i=r.findByProps("updateMessageRecord","createMessageRecord"),E=r.findByName("MessageRecord",!1),f=r.findByName("RowManager"),cooldowns={};n.push(d.before("dispatch",a.FluxDispatcher,([t])=>{if(t.type==="MESSAGE_UPDATE"){const e=c.get(t.channelId)?.get(t.message.id);if(!e||e.author?.bot||e.content===t.message.content)return;const id=e.id;if(cooldowns[id]&&Date.now()-cooldowns[id]<3000)return;t.message.__vml_lastEdit=Date.now();cooldowns[id]=Date.now();const clone={...e.toJS(),id:`editlog-${e.id}-${Date.now()}`,content:`[original] ${e.content}`,__vml_log:!0};a.FluxDispatcher.dispatch({type:"MESSAGE_CREATE",message:clone,channelId:t.channelId})}})),n.push(d.after("generate",f.prototype,([t],e)=>{if(t.rowType===1&&t.message.__vml_log){e.message.edited="original";e.backgroundHighlight??={};e.backgroundHighlight.backgroundColor=a.ReactNative.processColor("#facc1533");e.backgroundHighlight.gutterColor=a.ReactNative.processColor("#facc15ff")}})),n.push(d.after("createMessageRecord",i,function([t],e){if(t.__vml_log)e.__vml_log=!0})),n.push(d.after("default",E,([t],e)=>{e.__vml_log=!!t.__vml_log}));const m=()=>n.forEach(t=>t());return l.onUnload=m,l})({},vendetta.metro,vendetta.metro.common,vendetta.patcher,vendetta.plugin,vendetta.ui.components,vendetta.ui.assets,vendetta.storage);
+(function(l, r, a, d, o, _, g, u) {
+  "use strict";
+  const n = [], 
+        c = r.findByProps("_channelMessages"), 
+        i = r.findByProps("updateMessageRecord", "createMessageRecord"), 
+        E = r.findByName("MessageRecord", !1), 
+        f = r.findByName("RowManager");
+
+  n.push(d.before("dispatch", a.FluxDispatcher, ([t]) => {
+    if (t.type === "MESSAGE_UPDATE") {
+      const original = c.get(t.channelId)?.get(t.message.id);
+      if (!original) return;
+      if (original.author?.bot) return;
+      if (original.content === t.message.content) return;
+
+      const fakeId = `editlog-${original.id}-${Date.now()}`;
+      const clone = {
+        ...original.toJS(),
+        id: fakeId,
+        content: `[edited] ${original.content}`,
+        __vml_log: !0
+      };
+
+      a.FluxDispatcher.dispatch({
+        type: "MESSAGE_CREATE",
+        message: clone,
+        channelId: t.channelId
+      });
+    }
+  }));
+
+  n.push(d.after("generate", f.prototype, ([t], e) => {
+    if (t.rowType === 1 && t.message.__vml_log) {
+      e.message.edited = "previous version";
+      e.backgroundHighlight ??= {};
+      e.backgroundHighlight.backgroundColor = a.ReactNative.processColor("#facc1533");
+      e.backgroundHighlight.gutterColor = a.ReactNative.processColor("#facc15ff");
+    }
+  }));
+
+  n.push(d.after("createMessageRecord", i, function([t], e) {
+    if (t.__vml_log) e.__vml_log = !0;
+  }));
+  n.push(d.after("default", E, ([t], e) => {
+    e.__vml_log = !!t.__vml_log;
+  }));
+
+  const m = () => n.forEach(t => t());
+  return l.onUnload = m, l;
+})(_, vendetta.metro, vendetta.metro.common, vendetta.patcher, vendetta.plugin, vendetta.ui.components, vendetta.ui.assets, vendetta.storage);
